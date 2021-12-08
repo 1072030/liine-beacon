9d3442c80afc75d59585d1b06c624e8686018575
1072030 <46060471+1072030@users.noreply.github.com>
2021-12-08 16:31:12 +0800

    test reply beacon

--- line-beacon-backend/handleEvent/handleBeacon.js
+++ line-beacon-backend/handleEvent/handleBeacon.js
@@ -4,16 +4,17 @@ const handleBeacon = async (event, replyToken) => {
   let findUser;
   await mongoClient.connect().then(async () => {
     console.log(event);
+    //hwid event.beacon.hwid d
     findUser = await mongoClient
       .db("myFirstDatabase")
       .collection("beaconData")
       .findOne({ name: "fresh fruit" });
   });
-  // await client.pushMessage(event, {
-  //   type: "flex",
-  //   altText: "this is flex",
-  //   contents: findUser.contents,
-  // });
+  await client.pushMessage(event.source.userId, {
+    type: "flex",
+    altText: "this is flex",
+    contents: findUser.contents,
+  });
 };
 
 module.exports = handleBeacon;
