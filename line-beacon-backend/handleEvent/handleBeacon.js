142078b4ed48fc31b6e3760ecb6564f301356b47
1072030 <46060471+1072030@users.noreply.github.com>
2021-12-08 16:21:41 +0800

    add await

--- line-beacon-backend/handleEvent/handleBeacon.js
+++ line-beacon-backend/handleEvent/handleBeacon.js
@@ -8,7 +8,7 @@ const handleBeacon = async (event, replyToken) => {
       .collection("beaconData")
       .findOne({ name: "fresh fruit" });
   });
-  client.pushMessage(replyToken, {
+  await client.pushMessage(replyToken, {
     type: "flex",
     altText: "this is flex",
     contents: findUser.contents,
