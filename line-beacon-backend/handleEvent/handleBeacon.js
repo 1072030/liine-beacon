ae4cc2f7f7f192a77f640f000ff8501d29e3c65a
1072030 <46060471+1072030@users.noreply.github.com>
2021-12-08 17:08:31 +0800

    update

--- line-beacon-backend/handleEvent/handleBeacon.js
+++ line-beacon-backend/handleEvent/handleBeacon.js
@@ -8,16 +8,15 @@ const handleBeacon = async (event, replyToken) => {
     findUser = await mongoClient
       .db("myFirstDatabase")
       .collection("beaconData")
-      .find({ name: "fresh fruit" })
-      .sort({ $natural: -1 })
-      .limit(1);
+      .find({ name: "fresh fruit" }, { limit: 1 })
+      .sort({ $natural: -1 });
     console.log(findUser);
   });
 
   await client.pushMessage(event.source.userId, {
     type: "flex",
     altText: "this is flex",
-    contents: findUser[0].contents,
+    contents: findUser.contents,
   });
 };
 
