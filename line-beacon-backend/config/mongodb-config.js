affcec59c2ac391cc7327228d332c00da2fb3b22
1072030 <46060471+1072030@users.noreply.github.com>
2022-01-07 14:50:29 +0800

    update {}

--- line-beacon-backend/config/mongodb-config.js
+++ line-beacon-backend/config/mongodb-config.js
@@ -4,4 +4,4 @@ const { MongoClient } = require("mongodb");
 const url = process.env.Data_Base_Url;
 const mongoClient = new MongoClient(url);
 
-module.exports = mongoClient;
+module.exports = { mongoClient };
