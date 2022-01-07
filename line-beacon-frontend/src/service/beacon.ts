38b22f7c3eadef8c41ff47996e4a206f5c20fa08
1072030 <46060471+1072030@users.noreply.github.com>
2022-01-07 18:20:19 +0800

    rename

--- line-beacon-frontend/src/service/beacon.ts
+++ line-beacon-frontend/src/service/beacon.ts
@@ -1,5 +1,4 @@
 export async function beaconSetting(body: any) {
-  console.log("body", body);
   await fetch("https://beacon-test-bot.herokuapp.com/beacon", {
     method: "POST",
     body: JSON.stringify(body),
@@ -15,12 +14,13 @@ export async function beaconSetting(body: any) {
       console.log("result", result);
     });
 }
-export async function getHistoryData(userId: string) {
+export async function getRecord(userId: string) {
   const data = await fetch(
     `https://beacon-test-bot.herokuapp.com/getRecord?userId=${userId}`,
     {
       method: "GET",
       headers: {
+        "Access-Control-Allow-Origin": "*",
         "Content-Type": "application/json",
       },
     }
@@ -33,3 +33,22 @@ export async function getHistoryData(userId: string) {
     });
   return data;
 }
+// export async function getConpanyInfo(userId: string) {
+//   const data = await fetch(
+//     `https://beacon-test-bot.herokuapp.com/getRecord?userId=${userId}`,
+//     {
+//       method: "GET",
+//       headers: {
+//         "Access-Control-Allow-Origin": "*",
+//         "Content-Type": "application/json",
+//       },
+//     }
+//   )
+//     .then((res) => {
+//       return res.json();
+//     })
+//     .then((result) => {
+//       return result;
+//     });
+//   return data;
+// }
